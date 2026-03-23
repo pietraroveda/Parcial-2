aa6fb4b813d5aacaa297873375dcd27a74497341
pietraroveda <pietra.roveda@escola.pr.gov.br>
2026-03-23 08:04:38 -0300

    Add files via upload

---
+++ desafio3.py
@@ -0,0 +1,7 @@
+# Lista de 5 nomes
+nomes = ["Pietra", "Ana", "Eduarda", "Gabriela", "Vanessa"]  # Criando uma lista com 5 nomes
+
+# Imprimir todos os nomes da lista
+# Usamos um loop para iterar sobre cada nome na lista e imprimir
+for nome in nomes:  # A cada iteração, a variável 'nome' recebe o valor de um item da lista 'nomes'
+    print(nome)  # Exibe o nome atual da lista
